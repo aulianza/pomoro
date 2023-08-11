c83032d0e0cc6c890ff7a6897290d8fff61568c4
Ryan Aulia <hello@aulianza.id>
2023-08-11 08:59:23 +0700

    fix: update task item

--- src/modules/task/components/TaskItem.tsx
+++ src/modules/task/components/TaskItem.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import clsx from 'clsx';
-import { useState } from 'react';
-import { MdMoreVert as MoreIcon } from 'react-icons/md';
+import { useEffect, useState } from 'react';
+import { BsFillPlayCircleFill as StartIcon } from 'react-icons/bs';
 
 import BottomSheet from '@/common/components/BottomSheet';
 import Checkbox from '@/common/components/Checkbox';
@@ -28,6 +28,8 @@ const TaskItem = ({
   const [isCompleted, setCompleted] = useState(is_completed);
 
   const handleCompleteChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+
     setCompleted(event.target.checked);
 
     const updatedTask: TaskProps = {
@@ -45,21 +47,61 @@ const TaskItem = ({
     updateTask(updatedTask);
   };
 
+  const handleCheckboxClick = (event: React.MouseEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+  };
+
+  const handleStartTimer = (event: React.MouseEvent<SVGSVGElement>) => {
+    event.stopPropagation();
+
+    console.log('start timer');
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.keyCode === 27 || event.key === 'Escape') {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
   return (
     <>
-      <div className='border p-5 rounded-xl shadow-sm bg-white dark:bg-neutral-900 dark:border-neutral-700'>
+      <div
+        className='border p-5 rounded-xl shadow-sm bg-white dark:bg-neutral-900 dark:border-neutral-700 cursor-pointer'
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
         <div className='flex items-center justify-between'>
           <div className='w-11/12 flex items-center gap-4'>
-            <Checkbox checked={isCompleted} onChange={handleCompleteChange} />
-            <div className={clsx('text-[15px]', isCompleted && 'line-through')}>
+            <Checkbox
+              checked={isCompleted}
+              onChange={handleCompleteChange}
+              onClick={handleCheckboxClick}
+            />
+            <div
+              className={clsx(
+                'text-[15px] text-neutral-800 dark:text-neutral-200',
+                isCompleted && 'line-through',
+              )}
+            >
               {title}
             </div>
           </div>
-          <MoreIcon
-            onClick={() => setOpen(true)}
-            className='w-1/12 cursor-pointer'
-            size={20}
-          />
+          <div className='w-auto flex gap-2 items-center'>
+            <StartIcon
+              onClick={handleStartTimer}
+              className='cursor-pointer text-green-400'
+              size={20}
+            />
+          </div>
         </div>
       </div>
       <BottomSheet title='Edit Task' isOpen={isOpen} onClose={setOpen}>
