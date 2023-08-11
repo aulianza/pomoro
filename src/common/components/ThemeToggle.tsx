c744c2798bd095d13240250d4d390438f8ff2037
Ryan Aulia <hello@aulianza.id>
2023-08-11 08:23:29 +0700

    fix: render toggle dynamicly

--- src/common/components/ThemeToggle.tsx
+++ src/common/components/ThemeToggle.tsx
@@ -2,15 +2,22 @@
 
 import clsx from 'clsx';
 import { useTheme } from 'next-themes';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { BiMoon, BiSun } from 'react-icons/bi';
 
 const ThemeToggle = () => {
   const { resolvedTheme, setTheme } = useTheme();
+  const [themeColor, setThemeColor] = useState<string | undefined>(undefined);
 
   const handleChangeTheme = useCallback(() => {
     setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
   }, [resolvedTheme, setTheme]);
+
+  useEffect(() => {
+    setThemeColor(resolvedTheme);
+  }, [resolvedTheme]);
+
+  if (!themeColor) return null;
 
   return (
     <button
@@ -18,10 +25,11 @@ const ThemeToggle = () => {
         '!p-2 border border-neutral-400',
         'text-neutral-600 dark:text-neutral-400',
         'rounded-full transition hover:rotate-45',
+        'transition-all duration-300',
       )}
       onClick={handleChangeTheme}
     >
-      {resolvedTheme === 'light' ? <BiMoon size={22} /> : <BiSun size={22} />}
+      {themeColor === 'light' ? <BiMoon size={22} /> : <BiSun size={22} />}
     </button>
   );
 };
