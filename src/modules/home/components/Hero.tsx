96aa4ad7f90520dceb89ed97cffac1c4d9fe1f8d
Ryan Aulia <hello@aulianza.id>
2023-08-14 22:40:28 +0700

    fix: hero padding

--- src/modules/home/components/Hero.tsx
+++ src/modules/home/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <>
       <header className='sticky top-0 z-50'>
-        <div className='py-5 px-6 space-y-6 h-auto bg-white dark:bg-neutral-900'>
+        <div className='py-4 px-6 space-y-6 h-auto bg-white dark:bg-neutral-900'>
           <div className='flex justify-between items-center'>
             <div className='flex gap-2 items-center text-emerald-500 dark:text-emerald-400'>
               <LogoIcon size={28} />
