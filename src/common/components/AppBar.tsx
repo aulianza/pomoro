01e03551ba1ad7034bd470ec0e9581e121ff668f
Ryan Aulia <hello@aulianza.id>
2023-08-14 23:03:12 +0700

    fix: appbar menu color

--- src/common/components/AppBar.tsx
+++ src/common/components/AppBar.tsx
@@ -40,13 +40,13 @@ const AppBar = () => {
   const pathname = usePathname();
 
   return (
-    <div className='fixed bottom-0 w-full max-w-[480px] mx-auto  backdrop-blur-sm'>
-      <div className='flex gap-1 justify-center py-2 px-0 mx-4 md:mx-16 mb-6 rounded-full border dark:border-neutral-600 bg-white dark:bg-neutral-900 shadow-lg'>
+    <div className='fixed bottom-0 w-full max-w-[480px] mx-auto  backdrop-blur-'>
+      <div className='flex gap-1 justify-center py-2 px-0 mx-4 md:mx-16 mb-6 rounded-full border dark:border-neutral-600 bg-white dark:bg-neutral-900 shadow-lg text-neutral-600 dark:text-neutral-400'>
         {MENU.map((menu) => (
           <Link href={menu?.path} key={menu?.name}>
             <button
               className={clsx(
-                'py-3 px-5 flex items-center cursor-pointer gap-1 rounded-full dark:text-neutral-400',
+                'py-3 px-5 flex items-center cursor-pointer gap-1 rounded-full ',
                 'hover:text-neutral-700 dark:hover:text-neutral-300',
                 {
                   'bg-neutral-200 dark:bg-neutral-600 text-neutral-800 dark:text-neutral-50':
